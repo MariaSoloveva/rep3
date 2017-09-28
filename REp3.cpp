31fcb792d2329b3bcab95831f2c47d0628ffc9fb
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-28 22:59:14 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -3,17 +3,35 @@
 #include <math.h>
 
 int main(){
-    // Длина между двумя точками :
-    double x1;
-    std::cin >> x1;
-    double y1;
-    std::cin >> y1;
-    double x2;
-    std::cin >> x2;
-    double y2;
-    std::cin >> y2;
-    double s;
-    s = sqrt((x1-x2) * (x1-x2) +(y1-y2) * (y1-y2));
-    std::cout << s << std::endl;
+// Нахождение площади кольца :
+    std::cout << "Введите внутренний и внешний радиус" << std::endl;
+    double littleRadius;
+    std::cin >> littleRadius;
+    double bigRadius;
+    std::cin >> bigRadius;
+    double square = 3.14 * (bigRadius*bigRadius - littleRadius * littleRadius);
+    std::cout << square << std::endl;
+// Нахождение площади кольца :
+    std::cout << "Введите длину ребра" << std::endl;
+    double rib;
+    std::cin >> rib;
+    double volumeCube;
+    volumeCube = rib * rib * rib;
+    double surfaceArea;
+    surfaceArea = 6 * rib * rib;
+    std::cout << volumeCube << std::endl;
+    std::cout << surfaceArea << std::endl;
+// Нахождение объема квадрата :
+    std::cout << "Введите сторону квадрата, чтобы узнать периметр" << std::endl;
+    double side;
+    std::cin >> side;
+    double area = 4 * side;
+    std::cout << area << std::endl;
+// Нахождение радиуса окружности :
+    std::cout << "Введите радиус окружности, чтобы узнать диаметр" << std::endl;
+    double radius;
+    std::cin >> radius;
+    double deameter = 2 * radius;
+    std::cout << deameter << std::endl;
     return 0;
 }
