ef6e6e55d11f3032a3406f75e4c72e4ccdc3c27b
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-28 22:49:24 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -14,16 +14,5 @@ int main(){
     std::cin >> y2;
     double s = sqrt( ( x1-x2 ) * ( x1-x2 ) +( y1-y2 ) * ( y1-y2 ) );
     std::cout << s << std::endl;
-// Нахождение периметра равнобедренной трапеции :
-    std::cout << "Введите размеры большего и меньшего оснований и высоты" << std::endl;
-    double bigBase;
-    std::cin >> bigBase;
-    double littleBase;
-    std::cin >> littleBase;
-    double height;
-    std::cin >> height;
-    double sum = sqrt( height * height + ( bigBase - littleBase ) * ( bigBase - littleBase ) / 4 );
-    double perimeter = bigBase + littleBase + 2 * sum;
-    std::cout << perimeter << std::endl;
     return 0;
 }
