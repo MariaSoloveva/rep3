ab9dea98cfae765457135ef61a5e51c766ed2564
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-28 22:30:32 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -3,7 +3,15 @@
 #include <math.h>
 
 int main(){
+// Вывод числа Пи, округленного до сотых :
+    std::cout << "pi = 3.14" << std::endl;
 // Вывод чисел 1/13/49 c одним пробелом между ними :
-    std::cout << "1 13 49" << std::endl;    
+    std::cout << "1 13 49" << std::endl;
+//Вывод чисел 1/13/49 разделенные заданым символом :
+    std::cout << "Введите знак" << std::endl;
+    std::string sign1;
+    std::cin >> sign1;
+    std::string message = "1" + sign1 + "13"+sign1+"49";
+    std::cout << message << std::endl;
     return 0;
 }
