a6dab679b881b7717b171d39b440fdf9f53011dd
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-28 22:35:31 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -13,5 +13,23 @@ int main(){
     std::cin >> sign1;
     std::string message = "1" + sign1 + "13"+sign1+"49";
     std::cout << message << std::endl;
+    // Вывод трех любых чисел с двумя пробелами между ними :
+    std::cout << "Введите три любые числа" << std::endl;
+    std::string number1;
+    std::cin >> number1;
+    std::string number2;
+    std::cin >> number2;
+    std::string number3;
+    std::cin >> number3;
+    std::string message1 = number1 + "  " + number2 + "  " + number3;
+    std::cout << message1 << std::endl;
+// Вычисление значения функции1 :
+    std::cout << "Введите число а" << std::endl;
+    double a = 0;
+    std::cin >> a;
+    double x = 12 * a * a + 7 * a - 12;
+    double y = 3 * x * x * x + 4 * x * x - 11 * x + 1;
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
     return 0;
 }
