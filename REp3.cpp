3887c564173e6b2234055eedd48f1fa950265fd1
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-10-01 12:01:56 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -112,8 +112,8 @@ int main(){
     for (unsigned int x1 = 1; x1 <= 8; ++x1;){
         condition1 = (p.first + x1 == c) && (p.second + y1== d);
         condition2 = (p.first - x1 == c) && (p.second - y1== d);
-    	if (condition1 || condition2){ ++k;
-        } ++y1;
+    	if (condition1 || condition2){ ++k;}
+        ++y1;
     }
     if ((k > 0) || (p1.first == p2.first) || (p1.second == p2.second)){
         std::cout << "The queen from the bishop" << std::endl;
