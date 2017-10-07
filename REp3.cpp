3458e6da9c68d2b20a17f9935551c8f8953b9ee2
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-10-07 23:18:57 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -8,11 +8,9 @@ int main() {
     std::string nameOfTheCity = '';
     std::cout << "Enter Name of the City" << std::endl;
     getline(std::cin, nameOfTheCity);
-    if (nameOfTheCity.size() % 2 = 0)
-    {
+    if (nameOfTheCity.size() % 2 = 0) {
     	std::cout << "the number of digits is even" << std::endl;
-    } else
-    {
+    } else {
     	std::cout << "the number of digits is odd" << std:endl;
     }
 //Даны две фамилии. Определить, какая из них длиннее
@@ -21,11 +19,9 @@ int main() {
     getline(std::cin, surname1);
     td::string surname2 = '';
     getline(std::cin, surname2);
-    if (surname1.size() > surname2.size())
-    {
+    if (surname1.size() > surname2.size()) {
         std::cout << surname1 << " is longer" << std::endl;
-    } else
-    {
+    } else {
     	std::cout << surname2 << " is longer" << std::endl;
     }
  //Даны названия трех городов. Вывести на экран самое длинное и самое короткое название.
@@ -34,31 +30,21 @@ int main() {
     getline(std::cin, surname2);
     std::string surname3 = '';
     getline(std::cin, surname3);
-    if ((surname1.size() > surname2.size()) && (surname2.size() > surname3.size()))
-    {
+    if ((surname1.size() > surname2.size()) && (surname2.size() > surname3.size())) {
     	std::cout << "The longest is " << surname1 << " and the shortest is " << surname3 << std::endl;
-    } else
-    {
-    	if ((surname1.size() > surname3.size()) && (surname3.size() > surname2.size()))
-    	{
+    } else {
+    	if ((surname1.size() > surname3.size()) && (surname3.size() > surname2.size())) {
             std::cout << "The longest is " << surname1 << " and the shortest is " << surname2 << std::endl;
-    	} else
-    	{
-    		if ((surname2.size() > surname1.size()) && (surname1.size() > surname3.size()))
-    		{
+    	} else {
+    		if ((surname2.size() > surname1.size()) && (surname1.size() > surname3.size())) {
                 std::cout << "The longest is " << surname2 << " and the shortest is " << surname3 << std::endl;
-    		} else
-    		{
-    			if ((surname2.size() > surname3.size()) && (surname3.size() > surname1.size()))
-    			{
+    		} else {
+    			if ((surname2.size() > surname3.size()) && (surname3.size() > surname1.size())) {
     				std::cout << "The longest is " << surname2 << " and the shortest is " << surname1 << std::endl;
-    			} else
-    			{
-    				if ((surname3.size() > surname1.size()) && (surname1.size() > surname2.size()))
-    				{
+    			} else {
+    				if ((surname3.size() > surname1.size()) && (surname1.size() > surname2.size())) {
                         std::cout << "The longest is " << surname3 << " and the shortest is " << surname2 << std::endl;
-    				}else
-    				{
+    				}else {
                         std::cout << "The longest is " << surname3 << " and the shortest is " << surname1 << std::endl;
     				}
     			}
@@ -79,8 +65,7 @@ int main() {
 // Дано слово. Добавить к нему в начале и конце столько звездочек, сколько букв в этом слове.
     std::cout << "Enter a word" << std::endl;
     std::cin >> word;
-    for (insigned int i = 1, i <= word.size(), ++i)
-    {
+    for (insigned int i = 1, i <= word.size(), ++i) {
     	word.insert(0, '*');
     	word.push_back('*');
     }
@@ -90,8 +75,7 @@ int main() {
    std::string proposition = '';
    n = 0;
    getline(std::cin, proposition);
-   while (proposition.find('a') > 0)
-   {
+   while (proposition.find('a') > 0) {
         proposition.erase(0, proposition.find('a'));
         ++n;
    }
@@ -101,8 +85,7 @@ int main() {
     std::string str = 'Can you can a can as a canner can can a can?';
     std::cout << "Enter a word" << std::endl;
     std::string newWord = '';
-    while (str.find('can') >= 0)
-    {
+    while (str.find('can') >= 0) {
         std::cin >> newWord;
         str.replace(str.find('can'),str.find('can') + 3, newWord);
     }
