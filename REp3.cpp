00e457348beb8db2607636fe1e642a5df8da5dcd
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-14 19:02:56 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -10,10 +10,10 @@ void vectorToFile(const std::string& filename, std::vector<std::string>& data)
     for (int i = 0; i < data.size(); ++i)
     {
         fl << data[i];
-
         if (i < data.size() - 1)
             fl << std::endl;
     }
+    fl.close();
 }
 void vectorFromFile(const std::string& filename, std::vector<std::string>& data)
 {
@@ -61,22 +61,31 @@ std::string returnPart(std::string& str)
     buf[length] = '\0';
     return buf;
 }
+int returnNumber(std::string& str)
+{
+    int number = 0;
+    for (size_t j = 0; j < str.length(); ++j)
+    {
+        int digit = (int)str[j] - (int)'0';
+        number = number * 10 + digit;
+    }
+    return number;
+}
 void loadFromFile(const std::string& filename, std::vector<Book>& outData)
 {
     std::ifstream fl(filename);
     std::string str;
-    int i = 0;
     while (!fl.eof())
     {
         str.clear();
         std::getline(fl, str);
-        //outData[i].Author = buf;
-        std::cout << returnPart(str) << std::endl;
-        //outData[i].Title = str.copy(buf, 0, str.find("-"));
-        std::cout << returnPart(str) << std::endl;
-        std::cout << str << std::endl;
-        //outData[i].Year = std::stoi(str);
-        ++i;
+        Book Book1 =
+        {
+            returnPart(str),
+            returnPart(str),
+            returnNumber(str)
+        };
+        outData.push_back(Book1);
     }
 }
 enum Score
@@ -109,14 +118,53 @@ void saveToFileGroup(const std::string& filename, const Groups& groups)
                 fl << a.second << "/";
             }
         }
+        std::cout << std::endl;
     }
 }
+std::string returnPartToStudent(std::string& str)
+{
+    char buf[str.length()];
+    std::size_t length = str.copy(buf, str.find("/"), 0);
+    str = str.substr(str.find("/") + 1);
+    buf[length] = '\0';
+    return buf;
+}
+int returnNumberForGroup(std::string& str)
+{
+    std::string strHelp = returnPartToStudent(str);
+    return returnNumber(strHelp);
+}
+std::map<std::string,Score> returnRecordBook(std::string& str)
+{
+    std::map<std::string,Score> mp;
+    while(str.size() > 0)
+    {
+        std::string strHelp = returnPartToStudent(str);
+        mp.insert(std::pair<std::string, Score>(strHelp, Score(returnNumberForGroup(str))));
+    }
+    return mp;
+};
 void loadFromBook(const std::string& filename,Groups& groups)
 {
     std::ifstream fl(filename);
+    std::string str;
     while (!fl.eof())
     {
-
+        str.clear();
+        std::getline(fl, str);
+        std::map<std::string,Score> RecordBook;
+        std::string nameOfGroup = returnPartToStudent(str);
+        std::vector<Student> vecOfStudents;
+        {
+            Student Student1 =
+            {
+                returnPartToStudent(str),
+                returnNumberForGroup(str),
+                returnRecordBook(str)
+            };
+            vecOfStudents.push_back(Student1);
+        }
+        groups.insert(std::pair<std::string,std::vector<Student>>(nameOfGroup,vecOfStudents));
     }
 }
 void printVector(const std::vector<Book> vec)
@@ -128,25 +176,46 @@ void printVector(const std::vector<Book> vec)
         std::cout << vec[i].Year << std::endl;
     }
 }
+void printGroups(Groups& groups)
+{
+    for (const auto& e : groups)
+    {
+        std::cout << e.first << "/";
+        for (size_t i = 0; i < e.second.size(); ++i)
+        {
+            std::cout << e.second[i].Name << "/";
+            std::cout << e.second[i].Year << "/";
+            for (const auto& a : e.second[i].RecordBook)
+            {
+                std::cout << a.first << "/";
+                std::cout << a.second << "/";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
 int main()
 {
-    std::vector<std::string> vec = {"ef","kje","fqeif","u","niwe","fiuwenf","ienf"};
+    std::vector<std::string> vec = {"ef","kje","fqeif","u",
+                                    "niwe","fiuwenf","ienf"};
     vectorToFile("vec.txt", vec);
     std::vector<std::string> out;
     vectorFromFile("vec.txt", out);
     printVector(out);
-    std::vector<Book> vecOfBook = {{"Howard Phillips Lovecraft", "The Shadow Out of Time", 1934}, {"Edgar Allan Poe","The Fall of the House of Usher", 1839}};
+    std::vector<Book> vecOfBook = {{"Howard Phillips Lovecraft",
+        "The Shadow Out of Time", 1934}, {"Edgar Allan Poe",
+            "The Fall of the House of Usher", 1839}};
     saveToFileBook("structBook.txt", vecOfBook);
     std::vector<Book> vecOfBook1;
     saveToFileBook("structBook1.txt", vecOfBook);
     loadFromFile("structBook1.txt", vecOfBook1);
     printVector(vecOfBook1);
-    /*Groups groups1;
+    Groups groups1;
     std::string nameOfGroup = "iu8-11";
     std::map<std::string,Score> RecordBook = {{"Math", Good}, {"English", Good}};
     std::vector<Student> student;
     {
-        Student Student1 = 
+        Student Student1 =
         {
             "Soloveva Maria",
             1999,
@@ -156,6 +225,9 @@ int main()
     }
     groups1.insert(std::pair<std::string,std::vector<Student>>(nameOfGroup,student));
     saveToFileGroup("Group.txt", groups1);
-    loadFromBook("Group.txt",groups1);*/
+    Groups groups2;
+    loadFromBook("Group.txt",groups2);
+    saveToFileGroup("Group1.txt", groups2);
+    printGroups(groups2);
     return 0;
 }
