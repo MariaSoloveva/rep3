893eba61bc76ba779b0ce86b7ed183dc0bc55fc8
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-16 23:09:50 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -3,21 +3,13 @@
 #include <algorithm>
 #include <vector>
 
-void loadFromFile(const std::string& filename, std::vector<Book>& outData)
+void saveToFile(const std::string& filename, const std::vector<Book>& data)
 {
-
-    Book book1;
-    std::ifstream fl(filename);
-    std::string str;
-    while (!fl.eof())
+    std::fstream fl(filename, std::ios::out);
+    for (size_t i = 0; i < data.size() ; ++i)
     {
-        str.clear();
-        std::getline(fl, str);
-        book1.Author = str;
-        std::getline(fl, str);
-        book1.Title = str;
-        std::getline(fl, str);
-        book1.Year = atoi(str.c_str());
-        outData.push_back(book1);
+        fl << data[i].Author << std::endl;
+        fl << data[i].Title << std::endl;
+        fl << data[i].Year << std::endl;
     }
 }
