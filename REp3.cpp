edc5a084fc9e8e15fc6d641cbdc7019b7a94678c
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-16 23:40:11 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -1,15 +1,45 @@
-#include <iostream>
 #include <fstream>
-#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
 #include <vector>
 
-void saveToFile(const std::string& filename, const std::vector<Book>& data)
+struct Book
+{
+    std::string Author;
+    std::string Title;
+    int Year;
+};
+enum Score
+{
+    Unsatisfactorily = 2,
+    Satisfactorily,
+    Good,
+    Excellent
+};
+struct Student
+{
+    std::string Name;
+    int Year;
+    std::map<std::string, Score> RecordBook;
+};
+using Groups = std::map<std::string, std::vector<Student>>;
+void saveToFile(const std::string& filename, const Groups& groups)
 {
     std::fstream fl(filename, std::ios::out);
-    for (size_t i = 0; i < data.size() ; ++i)
+    for (const auto& e : groups)
     {
-        fl << data[i].Author << std::endl;
-        fl << data[i].Title << std::endl;
-        fl << data[i].Year << std::endl;
+        fl << e.first << std::endl;
+        for (size_t i = 0; i < e.second.size(); ++i)
+        {
+            fl << e.second[i].Name << std::endl;
+            fl << e.second[i].Year << std::endl;
+            for (const auto& a : e.second[i].RecordBook)
+            {
+                fl << a.first << std::endl;
+                fl << a.second << std::endl;
+            }
+        }
+        std::cout << std::endl;
     }
 }
