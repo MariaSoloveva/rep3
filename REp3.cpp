382d727ca1db764dbfad3ed005253749c226cd25
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-20 18:33:47 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -24,24 +24,40 @@ struct Student
     std::map<std::string, Score> RecordBook;
 };
 using Groups = std::map<std::string, std::vector<Student>>;
-void saveToFile(const std::string& filename, const Groups& groups)
+void loadFromFile(const std::string& filename, Groups& outGroups)
 {
-    std::fstream fl(filename, std::ios::out);
-    fl << groups.size() << std::endl;
-    for (const auto& e : groups)
-    {
-        fl << e.first << std::endl;
-        for (size_t i = 0; i < e.second.size(); ++i)
+    std::string nameOfGroup;
+    std::ifstream fl(filename);
+    while (!fl.eof()) {
+        int numberOfGroups = 0;
+        fl >> numberOfGroups;
+        for (int i = 0; i < numberOfGroups; ++i)
         {
-            fl << e.second[i].Name << std::endl;
-            fl << e.second[i].Year << std::endl;
-            fl << e.second[i].RecordBook.size() << std::endl;
-            for (const auto& a : e.second[i].RecordBook)
+            std::vector<Student> vecStudent;
+            std::getline(fl, nameOfGroup);
+            int numberOfStudents = 0;
+            fl >> numberOfStudents;
+            for (int j = 0; j < numberOfStudents; ++j)
             {
-                fl << a.first << std::endl;
-                fl << a.second << std::endl;
+                Student student;
+                std::getline(fl, student.Name);
+                std::string strYear;
+                std::getline(fl, strYear);
+                student.Year = atoi(strYear.c_str());
+                int scores = 0;
+                fl >> scores;
+                for (int k = 0; k < scores; ++k)
+                {
+                    std::string subjectName;
+                    std::getline(fl, subjectName);
+                    std::string grade;
+                    std::getline(fl, grade);
+                    student.RecordBook.insert(std::pair<std::string, Score>
+                                                      (subjectName, Score(atoi(grade.c_str()))));
+                }
+                vecStudent.push_back(student);
             }
+            outGroups.insert(std::pair<std::string, std::vector<Student>>(nameOfGroup, vecStudent));
         }
-        std::cout << std::endl;
     }
 }
