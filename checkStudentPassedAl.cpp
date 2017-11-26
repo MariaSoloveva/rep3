a0fb1c06afcba4ddf7a4e15cfd2a697c5394b145
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-26 17:22:08 +0300

    Update checkStudentPassedAl.cpp

--- checkStudentPassedAl.cpp
+++ checkStudentPassedAl.cpp
@@ -5,7 +5,8 @@ bool checkStudentPassedAl(std::vector<Student>& students)
     int numberOfBadStudents = 0;
     for (auto s : students)
     {
-        int cnt = std::count_if(s.RecordBook.begin(), s.RecordBook.end(), ratingGoodAtAl);
+        int cnt = std::count_if(s.RecordBook.begin(), s.RecordBook.end(),
+                                ratingGoodAtAl);
         if (cnt == 0)
             ++numberOfBadStudents;
     }
