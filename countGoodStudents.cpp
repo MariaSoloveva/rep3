698542ec4450d25746632b1e0cc6b90c3f729e69
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-26 17:22:36 +0300

    Update countGoodStudents.cpp

--- countGoodStudents.cpp
+++ countGoodStudents.cpp
@@ -5,7 +5,8 @@ int countGoodStudents(std::vector<Student>& students)
     int numberOfGoodStudents = 0;
     for (auto s : students)
     {
-        bool cnt = std::all_of(s.RecordBook.begin(), s.RecordBook.end(), ratingExcellent);
+        bool cnt = std::all_of(s.RecordBook.begin(), s.RecordBook.end(),
+                               ratingExcellent);
         if (cnt != 0)
         {
             ++numberOfGoodStudents;
