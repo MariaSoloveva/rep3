d14312ae027603c792f30221af634e11f7b4b806
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-26 17:17:18 +0300

    Update findAverageScores.cpp

--- findAverageScores.cpp
+++ findAverageScores.cpp
@@ -1,18 +1,17 @@
 #include "lab08.hpp"
 
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
-
-void loadFromFile(const std::string& filename, std::vector<std::string>& data)
+std::vector<float> findAverageScores(std::vector<Student>& group)
 {
-    std::ifstream fl(filename);
-    std::string str;
-    while (!fl.eof())
+    std::vector<float> averageScore;
+    int scores = 0;
+    for (size_t i = 0; i < group.size(); ++i)
     {
-        std::getline(fl, str);
-        data.push_back(str);
+        for (size_t j = 0; j < group[i].RecordBook.size(); ++j)
+        {
+            scores += group[i].RecordBook[j].Rating;
+         }
+        averageScore.push_back(scores / group[i].RecordBook.size());
+        scores = 0;
     }
+    return averageScore;
 }
