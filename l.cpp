d513d259fe8c67cf93129821b5b46a15330744d6
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-26 15:50:29 +0300

    Update l.cpp

--- l.cpp
+++ l.cpp
@@ -22,22 +22,13 @@ int main()
                 };
         group.push_back(Student1);
     }
-    std::vector<Course> RecordBook1 = {{"Math", Unsatisfactorily},
-                                       {"Al", Unsatisfactorily}, {"Ml", Unsatisfactorily}};
+    std::vector<Course> RecordBook1 = {{"Math", Unsatisfactorily}, {"Al", Unsatisfactorily}, {"Ml", Unsatisfactorily}};
     {
         Student Student2 =
                 {
                         RecordBook1
                 };
         group.push_back(Student2);
-    }
-    for (size_t i = 0; i < group.size(); ++i)
-    {
-        for (size_t j = 0; j < group[i].RecordBook.size(); ++j)
-        {
-            std::cout << group[i].RecordBook[j].Title << std::endl;
-            std::cout << group[i].RecordBook[j].Rating << std::endl;
-        }
     }
     std::cout << "Number of negligent students " << countNegligentStudents(group) << std::endl;
     std::cout << "Number of good students " << countGoodStudents(group) << std::endl;
@@ -46,11 +37,12 @@ int main()
     printVector(averageScores);
     std::cout << "Max average score : " << findMaxOfScores(averageScores) << std::endl;
     if (checkStudentFailedMl(group) == false)
-        std::cout << "Is there students that failed Ml? - No" << std::endl;
+        std::cout << "Is there any students that failed Ml? - No" << std::endl;
     else
-        std::cout << "Is there students that failed Ml? - Yes" << std::endl;
-    if (checkStudentPassedAl(group) == false)
-        std::cout << "Is there students that failed Al? - No" << std::endl;
+        std::cout << "Is there any students that failed Ml? - Yes" << std::endl;
+    if (checkStudentPassedAl(group) == true)
+        std::cout << "All students passed Al? - Yes" << std::endl;
     else
-        std::cout << "Is there students that failed Ml? - Yes" << std::endl;
+        std::cout << "All students passed Al? - No" << std::endl;
+    return 0;
 }
