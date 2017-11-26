7939ed4d530a9e5dc16b0c8249ce48bb8bda8ff9
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-26 17:21:23 +0300

    Update checkStudentFailedMl.cpp

--- checkStudentFailedMl.cpp
+++ checkStudentFailedMl.cpp
@@ -5,7 +5,8 @@ bool checkStudentFailedMl(std::vector<Student>& students)
     int numberOfBadStudents = 0;
     for (auto s : students)
     {
-        int cnt = std::count_if(s.RecordBook.begin(), s.RecordBook.end(), ratingUnsatisfactorilyAtMl);
+        int cnt = std::count_if(s.RecordBook.begin(), s.RecordBook.end(),
+                                ratingUnsatisfactorilyAtMl);
         if (cnt == 0)
             ++numberOfBadStudents;
     }
