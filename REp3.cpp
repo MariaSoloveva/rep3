7f1437eed9c5bd3b655f8a6c2c8b37f68a439e27
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-27 17:07:47 +0300

    Update REp3.cpp

--- REp3.cpp
+++ REp3.cpp
@@ -4,81 +4,64 @@
 #include <map>
 #include <string>
 #include <vector>
-#include "lab08.hpp"
-
-void printVector(std::vector<std::string>& vec)
+using Groups = std::map<std::string, std::vector<Student>>;
+void saveToFile(const std::string& filename, const Groups& groups)
 {
-    for (auto v : vec)
-        std::cout << v << " ";
-    std::cout << std::endl;
-}
-
-template<class K, class V>
-void printMap(const std::map<K, V> &m) {
-    for (const auto& e : m)
-        std::cout << e.first << ": " << e.second << std::endl;
-}
-void printVector(const std::vector<Book> vec)
-{
-    for (size_t i = 0; i < vec.size(); ++i)
-    {
-        std::cout << vec[i].Author << std::endl;
-        std::cout << vec[i].Title << std::endl;
-        std::cout << vec[i].Year << std::endl;
-    }
-}
-void printGroups(Groups& groups)
-{
+    std::fstream fl(filename, std::ios::out);
+    fl << groups.size() << std::endl;
     for (const auto& e : groups)
     {
-        std::cout << e.first << std::endl;
+        fl << e.first << std::endl;
+        fl << e.second.size() << std::endl;
         for (size_t i = 0; i < e.second.size(); ++i)
         {
-            std::cout << e.second[i].Name << std::endl;
-            std::cout << e.second[i].Year << std::endl;
+            fl << e.second[i].Name << std::endl;
+            fl << e.second[i].Year << std::endl;
+            fl << e.second[i].RecordBook.size() << std::endl;
             for (const auto& a : e.second[i].RecordBook)
             {
-                std::cout << a.first << std::endl;
-                std::cout << a.second << std::endl;
+                fl << a.first << std::endl;
+                fl << a.second << std::endl;
             }
         }
         std::cout << std::endl;
     }
 }
-int main()
+void loadFromFile(const std::string& filename, Groups& outGroups)
 {
-    std::vector<std::string> vec = {"ef", "kje", "fqeif", "u",
-                                    "niwe", "fiuwenf", "ienf"};
-    saveToFile("vec.txt", vec);
-    std::vector<std::string> out;
-    loadFromFile("vec.txt", out);
-    printVector(out);
-    std::vector<Book> vecOfBook = {{"Howard Phillips Lovecraft",
-                                           "The Shadow Out of Time", 1934}, {"Edgar Allan Poe",
-                                           "The Fall of the House of Usher", 1839}};
-    saveToFile("structBook.txt", vecOfBook);
-    std::vector<Book> vecOfBook1;
-    saveToFile("structBook1.txt", vecOfBook);
-    loadFromFile("structBook1.txt", vecOfBook1);
-    printVector(vecOfBook1);
-    Groups groups1;
-    std::string nameOfGroup = "iu8-11";
-    std::map<std::string, Score> RecordBook = {{"Math", Good}, {"English", Good}};
-    std::vector<Student> student;
+    std::ifstream fl(filename);
+    std::string strnumberOfGroups ;
+    std::getline(fl, strnumberOfGroups);
+    int numberOfGroups = atoi(strnumberOfGroups.c_str());
+    std::string nameOfGroup;
+    std::getline(fl, nameOfGroup);
+    for (int i = 0; i < numberOfGroups; ++i)
     {
-        Student Student1 =
-                {
-                        "Soloveva Maria",
-                        1999,
-                        RecordBook
-                };
-        student.push_back(Student1);
+        std::vector<Student> vecStudent;
+        std::string strnumberOfStudents ;
+        std::getline(fl, strnumberOfStudents);
+        int numberOfStudents = atoi(strnumberOfStudents.c_str());
+        for (int j = 0; j < numberOfStudents; ++j)
+        {
+            Student student;
+            std::getline(fl, student.Name);
+            std::string strYear;
+            std::getline(fl, strYear);
+            student.Year = atoi(strYear.c_str());
+            std::string strscores ;
+            std::getline(fl, strscores);
+            int scores = atoi(strscores.c_str());
+            for (int k = 0; k < scores; ++k)
+            {
+                std::string subjectName;
+                std::getline(fl, subjectName);
+                std::string grade;
+                std::getline(fl, grade);
+                student.RecordBook.insert(std::pair<std::string, Score>
+                                                 (subjectName, Score(atoi(grade.c_str()))));
+            }
+            vecStudent.push_back(student);
+        }
+        outGroups.insert(std::pair<std::string, std::vector<Student>>(nameOfGroup, vecStudent));
     }
-    groups1.insert(std::pair<std::string, std::vector<Student>>(nameOfGroup, student));
-    saveToFile("Group.txt", groups1);
-    Groups groups2;
-    loadFromFile("Group.txt", groups2);
-    saveToFile("Group1.txt", groups2);
-    printGroups(groups2);
-    return 0;
 }
