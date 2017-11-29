d8bf109293176d947d48b24874d441cfb1135702
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-29 18:56:46 +0300

    Update 4deleteMatrix.cpp

--- 4deleteMatrix.cpp
+++ 4deleteMatrix.cpp
@@ -6,6 +6,6 @@ void deleteMatrix(Matrix& matrix1)
 {
     for (int i = 0; i < matrix1.m; ++i)
         delete[]matrix1.matrix[i];
-    delete[]matrix1;
-    (matrix1.matrix) = nullptr;
+    //delete[]matrix1;
+    //(matrix1.matrix) = nullptr;
 }
