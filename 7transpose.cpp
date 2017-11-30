1d22d1ee0102f2aeece5fe31d678f6c807d2bc01
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-30 10:46:54 +0300

    Update 7transpose.cpp

--- 7transpose.cpp
+++ 7transpose.cpp
@@ -5,8 +5,8 @@
 Matrix transpose(const Matrix& matrix1)
 {
     Matrix matrix2 = newMatrix(matrix1.m, matrix1.n);
-    for (int i = 0; i < matrix1.n; ++i)
-        for (int j = 0; j < matrix1.m; ++j)
+    for (int i = 0; i < matrix1.m; ++i)
+        for (int j = 0; j < matrix1.n; ++j)
             matrix2.matrix[i][j] = matrix1.matrix[j][i];
     return matrix2;
 }
