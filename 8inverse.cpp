42025cb728be9a8170fd484e0f71679088733b82
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-11-30 21:28:09 +0300

    Update 8inverse.cpp

--- 8inverse.cpp
+++ 8inverse.cpp
@@ -2,6 +2,28 @@
 #include <cstring>
 #include "matrix.hpp"
 
-Matrix inverse(const Matrix&)
+Matrix inverse(const Matrix& matrix)
 {
+    Matrix obrMatrix = newMatrix(matrix.n, matrix.n);
+    float det = determinant(matrix);
+    if (det)
+    {
+        for (int i = 0; i < matrix.n; i++)
+        {
+            for (int j = 0; j < matrix.n; j++)
+            {
+                int m = matrix.n - 1;
+                Matrix tempMatr = newMatrix(m, m);
+                GetMatr(matrix, tempMatr, i, j);
+                obrMatrix.matrix[i][j] = pow(-1.0, i + j + 2) * determinant(tempMatr) / det;
+                deleteMatrix(tempMatr);
+            }
+        }
+        return obrMatrix;
+    }
+    else
+    {
+        std::cout << "Т.к. определитель матрицы = 0,\nто матрица вырожденная и обратной не имеет!!!" << std::endl;
+        return matrix;
+    }
 }
