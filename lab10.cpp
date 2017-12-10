0e08adfbd0a308d40419d4d805c5d6c1fc0012a9
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-12-10 14:59:52 +0300

    Create lab10.cpp

---
+++ lab10.cpp
@@ -0,0 +1,27 @@
+#include<algorithm>
+#include <iostream>
+#include "forward_list.hpp"
+
+int main()
+{
+    ForwardList* l = initList();
+    pushBack(l, 1);
+    pushBack(l, 2);
+    pushBack(l, 3);
+    pushFront(l, -1);
+    pushFront(l, -2);
+    pushFront(l, -3);
+    print(l);
+    std::cout << std::endl;
+    popFront(l);
+    insertAfter(l, l->Head, 100);
+    print(l);
+    std::cout << std::endl;
+    eraseAfter(l,l->Head);
+    print(l);
+    reverse(l);
+    std::cout << std::endl;
+    print(l);
+    destroyList(&l);
+    return 0;
+}
