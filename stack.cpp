f73febfa6dff34ae8194e761fb248d2d8fffbf64
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-12-11 14:05:04 +0300

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,52 @@
+#include<algorithm>
+#include <iostream>
+#include"stack.hpp"
+
+Stack* initStack()
+{
+    Stack* st = new Stack();
+    st->Head = nullptr;
+    return st;
+}
+void destroyS(Stack** st)
+{
+    Stack* stack = *st;
+    Stack::Node* tail = stack->Tail;
+    while (tail)
+    {
+        Stack::Node* tmp = tail->Next;
+        delete tail;
+        tail = tmp;
+    }
+    delete stack;
+    *st = nullptr;
+}
+Stack::Node* createNodeForStack(const Stack::value_type& data, Stack::Node* next)
+{
+    Stack::Node* node = new Stack::Node();
+    node->Data = data;
+    node->Next = next;
+    return node;
+}
+Stack::Node* push(Stack* const st, const Stack::value_type& data)
+{
+    Stack::Node* node = createNodeForStack(data, st->Head);
+    Stack::Node* head = st->Head;
+    st->Head = node;
+    st->Head->Next = head;
+    return node;
+}
+void pop (Stack* const st)
+{
+    Stack::Node* next = 0;
+    if (st->Head)
+    {
+        next = st->Head->Next;
+        delete st->Head;
+        st->Head = next;
+    }
+}
+Stack::Node* top(Stack* const st)
+{
+    return st->Head;
+}
