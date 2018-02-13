4629f01f23cb8dbdf19fdfbe6b0747a207f144ff
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-02-13 22:34:51 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,252 +1,10 @@
 #include <iostream>
-
-int StrLen(char* strIn);
-void StrCopy(char* strOut, char* strIn);
-bool StrCmp(char* strFirst, char* strSecond);
-
-class String
-{
-    char * Data;
- public:
-    /// <summary> Деструктор </summary>
-    ~String()
-    {
-        delete[] Data;
-    }
-    /// <summary> Конструктор по умолчанию </summary>
-    String()
-    {
-        Data = new char[1];
-        Data = nullptr;
-    }
-    /// <summary> Конструктор копирования </summary>
-    /// <param name="rhs">Объект, который копируем </param>
-    String(const String& rhs)
-    {
-        Data = new char[StrLen(rhs.Data)+1];
-        StrCopy(Data, rhs.Data);
-    }
-    /// <summary> Пользовательский конструктор </summary>
-    /// <param name="data">Данные, которые требуется поместить в создаваемый объект </param>
-    String(char* data)
-    {
-        Data = new char[StrLen(data)+1];
-        StrCopy(Data, data);
-    }
-    /// <summary> Оператор присваивания </summary>
-    /// <param name="data">Объект, который копируем </param>
-    /// <returns>Возвращаем ссылку на себя</returns>
-    String& operator=(const String& rhs)
-    {
-        if (this != &rhs)
-        {
-            delete[] this->Data;
-            this->Data = new char[StrLen(rhs.Data)+1];
-            StrCopy(this->Data, rhs.Data);
-        }
-        return *this;
-    }
-    /// <summary> Оператор += </summary>
-    /// <param name="rhs">Объект, который стоит после знака '+=' </param>
-    /// <returns>Возвращаем ссылку на себя</returns>
-    String& operator+=(const String& rhs)
-    {
-        int size = StrLen(this->Data) + StrLen(rhs.Data);
-        char* newString = new char[size + 1];
-        for (int i = 0; i < StrLen(this->Data); i++)
-            newString[i] = this->Data[i];
-        for (int k = StrLen(this->Data), j = 0; k <= size; k++, j++)
-            newString[k] = rhs.Data[j];
-        delete this->Data;
-        this->Data = newString;
-        return *this;
-    }
-    /// <summary> Оператор == </summary>
-    /// <param name="rhs">Объект, который стоит после знака '==' </param>
-    /// <returns>Возвращаем значения равенства двух строк</returns>
-    bool operator==(const String& rhs) const
-    {
-        return StrCmp(rhs.Data, Data);
-    }
-
-    /// <summary> Оператор &lt; </summary>
-    /// <param name="rhs">Объект, который стоит после знака "&lt;" </param>
-    /// <returns>Возвращаем значения сравнения двух строк</returns>
-    bool operator<(const String& rhs) const
-    {
-        return (StrLen(Data) < StrLen(rhs.Data)) ? true : false;
-    }
-    /// <summary> Функция поиска подстроки</summary>
-    /// <param name="substr">Подстрока, которую необходимо найти </param>
-    /// <returns>Возвращаем позицию substr. Если подстрока не найдена, то возвратить -1</returns>
-    size_t Find(const String& substr) const
-    {
-        size_t lenSub = StrLen(substr.Data);
-        if ((StrLen(Data) - lenSub) > 0)
-        {
-            for (size_t i = 0; i < (StrLen(Data) - lenSub); ++i)
-            {
-                int j = 0;
-                bool flag = true;
-                for (int k = i; k < lenSub; ++k)
-                {
-                    if (Data[k] != substr.Data[j])
-                        flag = false;
-                    ++j;
-                }
-                if (flag)
-                    return i;
-            }
-        }
-        else
-            return -1;
-    }
-
-    /// <summary> Функция замены букв </summary>
-    /// <param name="oldSymbol">Символ, который требуется заменить </param>
-    /// <param name="newSymbol">Символ, на который требуется заменить </param>
-    void Replace(char oldSymbol, char newSymbol)
-    {
-        for (int i = 0; Data[i] != 0; ++i)
-            if (Data[i] == oldSymbol)
-                Data[i] = newSymbol;
-    }
-
-    /// <summary> Функция возвращает длину строки </summary>
-    /// <returns>Возвращаем длину строки</returns>
-    size_t Size() const
-    {
-        size_t sizeOfData = 0;
-        for (; Data[sizeOfData] != 0; ++sizeOfData)
-            continue;
-        return sizeOfData;
-    }
-
-    /// <summary> Функция для определения пуста ли строка </summary>
-    bool Empty() const
-    {
-        return (Data == nullptr) ? true : false;
-    }
-
-    /// <summary> Оператор [] </summary>
-    /// <example>
-    /// <code>
-    /// String str = "some string";
-    /// char symbol = str[2]; // symbol == 'm'
-    /// </code>
-    /// </example>
-    /// <param name="index"> Индекс символа </param>
-    /// <returns> Значение символа в строке с индексом index</returns>
-    char operator[](size_t index) const
-    {
-        return (index >= 0 && index < StrLen(this->Data)) ? this->Data[index] : 0;
-    }
-    /// <summary> Оператор [] </summary>
-    /// <example>
-    /// <code>
-    /// String str = "some string";
-    /// str[0] = 'S'; // теперь переменная str равна "Some string"
-    /// </code>
-    /// </example>
-    /// <param name="index"> Индекс символа </param>
-    /// <returns> Ссылка на символ в строке с индексом index</returns>
-    char& operator[](size_t index)
-    {
-        static char emptyChar = '';
-        return (index >= 0 && index < StrLen(this->Data)) ? this->Data[index] : emptyChar;
-    }
-
-    /// <summary> Смотри пример </summary>
-    /// <example>
-    /// <code>
-    /// String str = "___some string___";
-    /// str.RTrim('_'); // теперь переменная str равна "___some string"
-    /// </code>
-    /// </example>
-    /// <param name="symbol"> Значение символов, которе отрезаем </param>
-    void RTrim(char symbol)
-    {
-        size_t sizeOfData = StrLen(Data);
-        for (; sizeOfData != -1; sizeOfData--)
-        {
-            if (Data[sizeOfData - 1] != symbol)
-                break;
-        }
-        std::cout << sizeOfData << std::endl;
-        char* newData = new char[sizeOfData + 1];
-        for (int i = 0; i < sizeOfData; ++i)
-            newData[i] = Data[i];
-        Data = newData;
-    }
-    /// <summary> Смотри пример </summary>
-    /// <example>
-    /// <code>
-    /// String str = "___some string___";
-    /// str.LTrim('_'); // теперь переменная str равна "some string___"
-    /// </code>
-    /// </example>
-    /// <param name="symbol"> Значение символов, которе отрезаем </param>
-    void LTrim(char* Data, char symbol)
-    {
-        size_t sizeOfData = 0;
-        for (; Data[sizeOfData] != 0; ++sizeOfData)
-        {
-            if (Data[sizeOfData] != symbol)
-                break;
-        }
-        char* newData = new char[StrLen(Data) - sizeOfData + 1];
-        int i = 0;
-        for (; Data[sizeOfData] != 0; ++sizeOfData)
-        {
-            newData[i] = Data[sizeOfData];
-            ++i;
-        }
-        Data = newData;
-    }
-    friend std::ostream& operator<<(std::ostream&, const String&);
-};
-
-/// <summary> Оператор +</summary>
-/// <example>
-/// <code>
-/// String a = "Hello";
-/// String b = "World";
-/// String c = a + b; // c равна "HelloWorld"
-/// </code>
-/// </example>
-/// <returns>Возвращаем строку равную a + b</returns>
-String operator+(const String& a, const String& b)
-{
-    String c = a;
-    return c += b;
-}
-
-/// <summary> Оператор !=</summary>
-bool operator!=(const String& a, const String& b)
-{
-    return !(StrCmp(a.Data, b.Data));
-}
-
-/// <summary> Оператор &gt;</summary>
-bool operator>(const String& a, const String& b)
-{
-    return (StrLen(a.Data) > StrLen(b.Data)) ? true : false;
-}
-
-/// <summary> Оператор вывода </summary>
-/// <param name="out">Поток куда выводим строку </param>
-/// <param name="str">Строка, которую выводим </param>
-/// <returns>Возвращаем ссылку на поток</returns>
-std::ostream& operator<<(std::ostream& out, const String& str)
-{
-    return out << str.Data;
-}
+#include "string.h"
 
 int StrLen(char* strIn)
 {
     int size = 0;
-    for (; strIn[size] != 0; size++)
-        continue;
+    for (; strIn[size] != 0; size++);
     return size;
 }
 void StrCopy(char* strOut, char* strIn)
@@ -257,10 +15,155 @@ void StrCopy(char* strOut, char* strIn)
 bool StrCmp(char* strFirst, char* strSecond)
 {
     int i = 0;
-    for (; (strFirst[i] == strSecond[i]) && (i < StrLen(strFirst)); ++i)
-        continue;
+    for (; (strFirst[i] == strSecond[i]) && (i < StrLen(strFirst)); ++i);
     return (i == StrLen(strFirst)) ? true : false;
 }
+
+String::~String()
+{
+    delete[] Data;
+}
+String::String() : Data(nullptr) {}
+String::String(const String& rhs)
+{
+    Data = new char[StrLen(rhs.Data)+1];
+    StrCopy(Data, rhs.Data);
+}
+String::String(char* data)
+{
+    Data = new char[StrLen(data)+1];
+    StrCopy(Data, data);
+}
+String& String::operator=(const String& rhs)
+{
+    if (this != &rhs)
+    {
+        delete[] this->Data;
+        this->Data = new char[StrLen(rhs.Data)+1];
+        StrCopy(this->Data, rhs.Data);
+    }
+    return *this;
+}
+String& String::operator+=(const String& rhs)
+{
+    int size = StrLen(this->Data) + StrLen(rhs.Data);
+    char* newString = new char[size + 1];
+    for (int i = 0; i < StrLen(this->Data); i++)
+        newString[i] = this->Data[i];
+    for (int k = StrLen(this->Data), j = 0; k <= size; k++, j++)
+        newString[k] = rhs.Data[j];
+    delete this->Data;
+    this->Data = newString;
+    return *this;
+}
+bool String::operator<(const String& rhs) const
+{
+    return (StrLen(Data) < StrLen(rhs.Data)) ? true : false;
+}
+bool String::operator==(const String& rhs) const
+{
+    return StrCmp(rhs.Data, Data);
+}
+size_t String::Find(const String& substr) const
+{
+    size_t lenSub = StrLen(substr.Data);
+    if ((StrLen(Data) - lenSub) > 0)
+    {
+        for (size_t i = 0; i < (StrLen(Data) - lenSub); ++i)
+        {
+            int j = 0;
+            bool flag = true;
+            for (int k = i; k < lenSub; ++k)
+            {
+                if (Data[k] != substr.Data[j])
+                    flag = false;
+                ++j;
+            }
+            if (flag)
+                return i;
+        }
+    }
+    else
+        return -1;
+}
+void String::Replace(char oldSymbol, char newSymbol)
+{
+    for (int i = 0; Data[i] != 0; ++i)
+        if (Data[i] == oldSymbol)
+            Data[i] = newSymbol;
+}
+size_t String::Size() const
+{
+    size_t sizeOfData = 0;
+    for (; Data[sizeOfData] != 0; ++sizeOfData);
+    return sizeOfData;
+}
+bool String::Empty() const
+{
+    return (Data == nullptr) ? true : false;
+}
+char String::operator[](size_t index) const
+{
+    return (index >= 0 && index < StrLen(this->Data)) ? this->Data[index] : 0;
+}
+char& String::operator[](size_t index)
+{
+    static char emptyChar = '';
+    //std::cerr << "Index out of range. \n";
+    return (index >= 0 && index < StrLen(this->Data)) ? this->Data[index] : emptyChar;
+}
+void String::RTrim(char symbol)
+{
+    size_t sizeOfData = StrLen(Data);
+    for (; sizeOfData != -1; sizeOfData--)
+    {
+        if (Data[sizeOfData - 1] != symbol)
+            break;
+    }
+    std::cout << sizeOfData << std::endl;
+    char* newData = new char[sizeOfData + 1];
+    for (int i = 0; i < sizeOfData; ++i)
+        newData[i] = Data[i];
+    Data = newData;
+}
+void String::LTrim(char* Data, char symbol)
+{
+    size_t sizeOfData = 0;
+    for (; Data[sizeOfData] != 0; ++sizeOfData)
+    {
+        if (Data[sizeOfData] != symbol)
+            break;
+    }
+    char* newData = new char[StrLen(Data) - sizeOfData + 1];
+    int i = 0;
+    for (; Data[sizeOfData] != 0; ++sizeOfData)
+    {
+        newData[i] = Data[sizeOfData];
+        ++i;
+    }
+    Data = newData;
+}
+
+String String::operator+(const String& a, const String& b)
+{
+    String c = a;
+    return c += b;
+}
+
+bool String::operator!=(const String& a, const String& b)
+{
+    return !(StrCmp(a.Data, b.Data));
+}
+
+bool String::operator>(const String& a, const String& b)
+{
+    return (StrLen(a.Data) > StrLen(b.Data)) ? true : false;
+}
+std::ostream& operator<<(std::ostream& out, const String& str)
+{
+    return out << str.Data;
+}
+
 int main()
 {
 }
