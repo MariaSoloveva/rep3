bb6e4236c715f90bedf2afa211b26920fb69e2df
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-02-15 10:18:15 +0300

    Update gcdex.cpp

--- gcdex.cpp
+++ gcdex.cpp
@@ -1,12 +1,13 @@
-int gcdex (int a, int b, int & x, int & y) {
+int gcdex(int a, int b, int & x, int & y)
+{
 	if (a == 0)
-  {
-		x = 0;
-    y = 1;
-		return b;
+        {
+	    x = 0;
+            y = 1;
+	    return b;
 	}
 	int x1 = 0;
-  int y1 = 0;
+        int y1 = 0;
 	int d = gcdex(b%a, a, x1, y1);
 	x = y1 - (b / a) * x1;
 	y = x1;
