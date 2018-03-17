0a92f8820a407a9b1ccacfe555bf01eebe106822
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-03-17 21:42:29 +0300

    Update res.cpp

--- res.cpp
+++ res.cpp
@@ -1,87 +1,156 @@
 #include <iostream>
-#include <stdexcept>
-#include "fsm.hpp"
 
-
-struct MyEx
+template <class T>
+class BinarySearchTree
 {
-    const char* message;
-    explicit MyEx(const char* m)
-            : message(m)
-    { }
-
-    const char* what() const
+public:
+    struct Node
     {
-        return message;
+        Node* Parent;
+        Node* Left;
+        Node* Right;
+        T Value;
+    };
+    BinarySearchTree()
+    {
+        Root = NULL;
     }
+    ~BinarySearchTree()
+    {
+       delete[] Root;
+    }
+
+    Node* createNode(T value, Node *parent)
+    {
+        Node* tmp = new Node;
+        tmp->Left = tmp->Right = NULL;
+        tmp->Value = value;
+        tmp->Parent = parent;
+        return tmp;
+    }
+    void Add(const T& value)
+    {
+        Node* root = Root;
+        if (Root == NULL)
+        {
+            Root = createNode(value, Root);
+            return;
+        }
+        else
+        {
+            Node *where = Root;
+            while (where)
+            {
+                if (value <= Root->Value && where->Left == NULL)
+                {
+                    where->Left = createNode(value, where);
+                    return;
+                } else if (value > Root->Value && where->Right == NULL)
+                {
+                    where->Right = createNode(value, where);
+                    return;
+                }
+                if (value <= where->Value)
+                   where = where->Left;
+                else
+                    where = where->Right;
+            }
+        }
+    }
+    Node* Find(const T& value)
+    {
+        Node * where = Root;
+        while (where)
+        {
+            if (where->Value > value)
+            {
+                where = where->Left;
+                continue;
+            }
+            else if (where->Value < value)
+            {
+                where = where->Right;
+                continue;
+            }
+            else
+            {
+                return where;
+            }
+        }
+        return NULL;
+    }
+    void Remove(Node* node)
+    {
+        Node* where = node;
+        if (where->Left && where->Right)
+        {
+            Node* root = where;
+            while (root->Right)
+            {
+                    root = root->Right;
+            }
+            where->Value = root->Value;
+            Remove(root);
+            return;
+        }
+        else if (where->Right)
+        {
+            if (where == where->Parent->Right)
+                where->Parent->Right = where->Right;
+            else
+                where->Parent->Left = where->Right;
+        }
+        else if (where->Left)
+        {
+            if (where == where->Parent->Left)
+                where->Parent->Left->Value = where->Value;
+            else
+                where->Parent->Right->Value = where->Left->Value;
+        }
+        else
+        {
+            if (where->Parent->Right == node)
+            {
+                where->Parent->Right = NULL;
+            }
+            else
+                where->Parent->Left = NULL;
+        }
+    }
+private:
+    Node* Root;
 };
-
-#define EXPECT_TRUE(x) if (!(x)) { throw MyEx(#x); }
-#define EXPECT_FALSE(x) if ((x)) { throw MyEx(#x); }
-#define EXPECT_EXCEPTION(x) try { (x); throw MyEx(#x); } catch(std::exception&) { }
-
-enum class Action
+template <class T>
+bool isBST(BinarySearchTree<T>::Node* root)
 {
-    Finding,
-    Attaking,
-    FindingAid,
-    Killed
-};
+    BinarySearchTree<T>::Node* rootCopy = root;
+    if (root == nullptr)
+        return true;
+    else
+    {
+        while (root)
+        {
+            if (root->Right->Value <= root->Value || root->Left->Value > root->Value)
+                return false;
+            root = root->Left;
+            rootCopy = rootCopy->Right;
+            return isBST(root) && isBST(rootCopy);
+        }
+    }
+}
 
 int main()
 {
-    FiniteStateMachine<std::string, std::string, Action> fa;
-
-    std::map<std::string, std::string> m1 = {
-            {"I've found it", "attacks"}
-    };
-
-    std::map<std::string, std::string> m2 = {
-            {"killed the purpose", "looking for the purpose of"},
-            {"there are few lives", "looking for a first aid kit"}
-    };
-
-    std::map<std::string, std::string> m3 = {
-            {"I've found it", "looking for the purpose of"},
-            {"ended life", "killed"}
-    };
-
-    std::map<std::string, std::string> m4;
-    fa.AddState("looking for the purpose of", Action::Finding, m1)
-            .AddState("Attaking", Action::Attaking, m2)
-            .AddState("looking for a first aid kit", Action::FindingAid, m3)
-            .AddState("killed", Action::Killed, m4);
-
-    fa.SetState("looking for the purpose of");
-
-    EXPECT_TRUE(fa("I've found it") == Action::Attaking);
-    /*EXPECT_TRUE(fa("killed the purpose") == Action::Finding);
-    EXPECT_TRUE(fa("I've found it") == Action::Attaking);
-    EXPECT_TRUE(fa("there are few lives") == Action::FindingAid);
-    // из состояния 'ищет аптечку' нет перехода по входу "убил цель"
-    // поэтому генерируем исключение
-    EXPECT_EXCEPTION(fa("killed the purpose"));
-    EXPECT_TRUE(fa("I've found it") == Action::Finding);
-    EXPECT_TRUE(fa("I've found it") == Action::Attaking);
-    EXPECT_TRUE(fa("there are few lives") == Action::FindingAid);
-    EXPECT_TRUE(fa("ended life") == Action::Killed);
-
-    EXPECT_EXCEPTION(fa("I've found it"));
-
-    SaveToFile("fa", fa);
-
-    FiniteStateMachine<std::string, std::string, Action> copy;
-    LoadFromFile("fa", copy);
-
-
-    EXPECT_TRUE(copy("I've found it") == Action::Attaking);
-    EXPECT_TRUE(copy("killed the purpose") == Action::Finding);
-    EXPECT_TRUE(copy("I've found it") == Action::Attaking);
-    EXPECT_TRUE(copy("there are few lives") == Action::FindingAid);
-    EXPECT_EXCEPTION(copy("killed the purpose"));
-    EXPECT_TRUE(copy("I've found it") == Action::Finding);
-    EXPECT_TRUE(copy("I've found it") == Action::Attaking);
-    EXPECT_TRUE(copy("there are few lives") == Action::FindingAid);
-    EXPECT_TRUE(copy("ended life") == Action::Killed);
-    EXPECT_EXCEPTION(copy("I've found it"));*/
+    BinarySearchTree<int> tree;
+    BinarySearchTree<int>::Node* x;
+    tree.Add(4);
+    tree.Add(5);
+    tree.Add(3);
+    tree.Add(2);
+    tree.Add(6);
+    x = tree.Find(5);
+    tree.Remove(x);
+    x = tree.Find(6);
+    tree.Remove(x);
+    return 0;
 }
