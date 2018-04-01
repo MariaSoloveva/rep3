76da42742b1be18138f7061c50388f5c53399aab
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-01 14:14:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ int main()
     std::cout << std::endl;
     vector<int> a1(10, 0);
     vector<int> b(10, 0);
-    /*vector<int> c;
-    c = a;
+    vector<int> c;
+    /*c = a;
     std::cout << a.at(0) << " " << b.at(1)  << " " << b.at(b.size() - 1)<< std::endl;
     std::cout << a[1] << " " << b[1] << std::endl;
     a[1] = 11;
