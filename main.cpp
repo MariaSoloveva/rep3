3f83fc9141b312601336d5b20b59916b30f25f32
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-09 20:02:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main()
     std::cout << "2 != 3  " << (stringThird != stringSecond) << std::endl;
     stringSecond.Replace('s', 'S');
     std::cout << "2 replace 's' -> 'S' " << stringSecond << std::endl;
-    //  std::cout << "find third string in second position"
+    /*  std::cout << "find third string in second position"
               << stringSecond.Find(stringThird) << std::endl;
     stringSecond.Replace('S', 's');
     std::cout << "2 replace 'S' -> 's' " << stringSecond << std::endl;
