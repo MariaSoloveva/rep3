c64b39a47f9b124ddd9906550a65a6becb2c1239
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-14 07:43:08 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "bst.hpp"
+
+int main()
+{
+    BinarySearchTree<int> tree;
+    BinarySearchTree<int>::Node* x;
+    tree.Add(4);
+    tree.Add(5);
+    tree.Add(3);
+    tree.Add(2);
+    tree.Add(6);
+    x = tree.Find(5);
+    tree.Remove(x);
+    x = tree.Find(6);
+    tree.Remove(x);
+    x = tree.Find(4);
+    std::cout << tree.isBST(x) << std::endl;
+    return 0;
+}
