954b29d684af08aac1523271ca2d8771a6e61f6e
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-14 09:55:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include "bst.hpp"
 
+
 int main()
 {
     BinarySearchTree<int> tree;
     BinarySearchTree<int>::Node* x;
     tree.Add(4);
     tree.Add(5);
-    tree.Add(3);
+    /*  tree.Add(3);
     tree.Add(2);
     tree.Add(6);
     x = tree.Find(5);
-    /*tree.Remove(x);
+    tree.Remove(x);
     x = tree.Find(6);
     tree.Remove(x);
     x = tree.Find(4);
