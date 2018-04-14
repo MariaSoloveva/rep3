149425554aa4f6962876a32f5b2b1a5fa79108ed
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-14 10:16:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main()
     tree.Add(5);
     tree.Add(3);
     tree.Add(2);
-    tree.Add(6);
-    /*x = tree.Find(5);
+    /*tree.Add(6);
+    x = tree.Find(5);
     tree.Remove(x);
     x = tree.Find(6);
     tree.Remove(x);
