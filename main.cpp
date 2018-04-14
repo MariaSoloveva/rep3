ee0926e325c19507b443b45509a03145f006bb27
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-14 10:42:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ int main()
     tree.Remove(x);
     x = tree.Find(5);
     tree.Remove(x);
-    x = tree.Find(4);
-    //  std::cout << tree.isBST(x) << std::endl;
+    std::cout << tree.isBST(x) << std::endl;
     return 0;
 }
