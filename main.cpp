fc6b5d5bf04e7c4442e70256ff658ab25b3187e1
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-15 00:29:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#includ"bst.hpp"
 
-
-template <class T>
+/*template <class T>
 class set
 {
     struct Node
@@ -455,5 +455,27 @@ int main()
     //  set<int> s2;
     //  s2 = s1;
     //  s1.erase(s1.find(19));
+    return 0;
+}*/
+int main()
+{
+    BinarySearchTree<int> tree;
+    BinarySearchTree<int>::Node* x;
+    tree.Add(20);
+    tree.Add(15);
+    tree.Add(10);
+    tree.Add(11);
+    tree.Add(16);
+    tree.Add(25);
+    tree.Add(30);
+    tree.Add(24);
+    tree.Add(35);
+    tree.Add(31);
+    /*x = tree.Find(11);
+    tree.Remove(x);
+    x = tree.Find(6);
+    tree.Remove(x);
+    x = tree.Find(4);
+    std::cout << tree.isBST(x) << std::endl;*/
     return 0;
 }
