abd2fc0fc1ac8b29499076ddddc1fbc228abcb9d
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-14 22:10:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -438,12 +438,12 @@ int main()
     s1.insert(11);
     s1.insert(22);
     s1.insert(19);
-    std::cout<< s1.size() <<std::endl;
+    /*std::cout<< s1.size() <<std::endl;
     std::cout<< s1.empty() <<std::endl;
     s1.find(9);
     std::cout<< s1.has(9) <<std::endl;
     //  set<int> s2;
     //  s2 = s1;
-    s1.erase(s1.find(19));
+    s1.erase(s1.find(19));*/
     return 0;
 }
