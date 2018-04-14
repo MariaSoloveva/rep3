c5c7df473b8b6783bfdd6df88df3e15677286d67
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-15 01:08:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include"bst.hpp"
 
+int main()
+{
+    BinarySearchTree<int> tree;
+    BinarySearchTree<int>::Node* x;
+    tree.Add(20);
+    tree.Add(15);
+    tree.Add(10);
+    tree.Add(11);
+    tree.Add(16);
+    tree.Add(25);
+    tree.Add(30);
+    tree.Add(24);
+    tree.Add(35);
+    tree.Add(31);
+    x = tree.Find(11);
+    tree.Remove(x);
+    /*  x = tree.Find(6);
+    tree.Remove(x);
+    x = tree.Find(4);
+    std::cout << tree.isBST(x) << std::endl;*/
+    return 0;
+}
 
 /*template <class T>
 class set
@@ -458,25 +480,3 @@ int main()
     //  s1.erase(s1.find(19));
     return 0;
 }*/
-int main()
-{
-    BinarySearchTree<int> tree;
-    BinarySearchTree<int>::Node* x;
-    tree.Add(20);
-    tree.Add(15);
-    tree.Add(10);
-    tree.Add(11);
-    tree.Add(16);
-    tree.Add(25);
-    tree.Add(30);
-    tree.Add(24);
-    tree.Add(35);
-    tree.Add(31);
-    x = tree.Find(11);
-    tree.Remove(x);
-    /*  x = tree.Find(6);
-    tree.Remove(x);
-    x = tree.Find(4);
-    std::cout << tree.isBST(x) << std::endl;*/
-    return 0;
-}
