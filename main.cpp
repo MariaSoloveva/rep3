f89a8deea90fbd29b194b3ca21c03afc6683a08e
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-15 11:31:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include"bst.hpp"
 
-
 int main()
 {
     BinarySearchTree<int> tree;
@@ -18,9 +17,9 @@ int main()
     tree.Add(31);
     x = tree.Find(11);
     //  tree.Remove(x);
-    /*x = tree.Find(6);
-    tree.Remove(x);
+    x = tree.Find(6);
+    //  tree.Remove(x);
     x = tree.Find(4);
-    std::cout << tree.isBST(x) << std::endl;*/
+    std::cout << tree.isBST(x) << std::endl;
     return 0;
 }
