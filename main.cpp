daffa007aa1fdbe4b2e2fb2362e04c1fd789a71f
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-04-15 12:35:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-#include"bst.hpp"
+#include "set.hpp"
 
 int main()
 {
-    BinarySearchTree<int> tree;
-    BinarySearchTree<int>::Node* x;
-    tree.Add(20);
-    tree.Add(15);
-    tree.Add(10);
-    tree.Add(11);
-    tree.Add(16);
-    tree.Add(25);
-    tree.Add(30);
-    tree.Add(24);
-    tree.Add(35);
-    tree.Add(31);
-    x = tree.Find(11);
-    //  tree.Remove(x);
-    x = tree.Find(6);
-    //  tree.Remove(x);
-    x = tree.Find(4);
-    std::cout << tree.isBST(x) << std::endl;
+    set<int> s1;
+    s1.insert(10);
+    s1.insert(5);
+    s1.insert(3);
+    s1.insert(2);
+    s1.insert(8);
+    s1.insert(0);
+    s1.insert(15);
+    s1.insert(20);
+    s1.insert(11);
+    s1.insert(22);
+    s1.insert(19);
+    std::cout<< s1.size() <<std::endl;
+    std::cout<< s1.empty() <<std::endl;
+    s1.find(9);
+    std::cout<< s1.has(9) <<std::endl;
+    set<int> s2(s1);
+    //  s1.erase(s1.find(19));
+    //  set<int> s2;
+    //  s2 = s1;
     return 0;
 }
