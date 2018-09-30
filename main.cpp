cf57aeb92a49f04a315b20da3ab745b23a2a0d1c
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-09-30 19:25:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,5 @@
 #include <string>
 
 int main() {
-    std::string s = "{
-    "lastname" : "Ivanov",
-    "firstname" : "Ivan",
-    "age" : 25,
-    "islegal" : false,
-    "marks" : [
-     4, 5, 5, 5, 2, 3
-    ],
-    "address" : {
-        "city" : "Moscow",
-        "street" : "Vozdvijenka"
-    }
-}";
-    Json str(s);
     return 0;
 }
