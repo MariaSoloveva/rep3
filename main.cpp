2d014258191d3acfb8d9abbfff94c42e7413bb13
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-09-30 19:47:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include "sharedptr.hpp"
 #include <string>
 
-int main() {
+int main()
+{
+    SharedPtr<std::string> ptr;
+    SharedPtr<std::string> ptr2;
+    ptr.reset();
+    ptr2.swap(ptr);
+    swap(ptr, ptr2);
+    std::cout << !ptr << std::endl;
+    std::cout << bool(ptr) << std::endl;
+    std::cout << ptr2.get() << std::endl;
+    std::cout << get_pointer(ptr2) << std::endl;
+    std::cout << (ptr != nullptr) << std::endl;
+    std::cout << (nullptr != ptr) << std::endl;
+    std::cout << (nullptr == ptr2) << std::endl;
+    std::cout << (ptr2 == nullptr) << std::endl;
     return 0;
 }
