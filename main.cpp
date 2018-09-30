9877abf1ac8c2780c298b54b0b9d134153199371
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-09-30 19:50:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int main()
     std::cout << !ptr << std::endl;
     std::cout << bool(ptr) << std::endl;
     std::cout << ptr2.get() << std::endl;
-    std::cout << (ptr != nullptr) << std::endl;
-    std::cout << (nullptr != ptr) << std::endl;
-    std::cout << (nullptr == ptr2) << std::endl;
-    std::cout << (ptr2 == nullptr) << std::endl;
+    std::cout << (ptr != nullptr_t) << std::endl;
+    std::cout << (nullptr_t != ptr) << std::endl;
+    std::cout << (nullptr_t == ptr2) << std::endl;
+    std::cout << (ptr2 == nullptr_t) << std::endl;
     return 0;
 }
