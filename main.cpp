6158f4354472b348ed135ad9e2059ae5faddd746
Maria Soloveva <32188528+MariaSoloveva@users.noreply.github.com>
2018-10-06 21:32:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include "sharedptr.hpp"
-#include <string>
 
-int main()
-{
-    SharedPtr<std::string> ptr;
-    SharedPtr<std::string> ptr2;
-    ptr.reset();
-    ptr2.swap(ptr);
-    swap(ptr, ptr2);
-    std::cout << !ptr << std::endl;
-    std::cout << bool(ptr) << std::endl;
-    std::cout << ptr2.get() << std::endl;
-    return 0;
-}
